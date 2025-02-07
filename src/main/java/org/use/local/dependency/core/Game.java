169c06462449c15deeb9dfc1886ad1804c3f9125
JP101TEST <job.peer2012@gmail.com>
2025-02-07 15:28:08 +0700

    Setup 0.1.0

--- src/main/java/org/use/local/dependency/core/Game.java
+++ src/main/java/org/use/local/dependency/core/Game.java
@@ -1,35 +1,37 @@
 package org.use.local.dependency.core;
 
-import com.raylib.Camera3D;
-import com.raylib.Vector3;
 import lombok.extern.log4j.Log4j2;
 
 import static com.raylib.Raylib.*;
-import static com.raylib.Raylib.CameraMode.CAMERA_ORBITAL;
-import static com.raylib.Raylib.CameraProjection.CAMERA_PERSPECTIVE;
 
 @Log4j2
 public class Game {
-    public void run(){
+    public void run() {
         log.info("Game start");
-        initWindow(800, 450, "Demo");
-        setTargetFPS(60);
-        Camera3D camera = new Camera3D(new Vector3(18,16,18),
-                new Vector3(),
-                new Vector3(0,1,0),
-                45, CAMERA_PERSPECTIVE);
-
-        while (!windowShouldClose()) {
-            updateCamera(camera, CAMERA_ORBITAL);
-            beginDrawing();
-            clearBackground(RAYWHITE);
-            beginMode3D(camera);
-            drawGrid(20, 1.0f);
-            endMode3D();
-            drawText("Hello world", 190, 200, 20, VIOLET);
-            drawFPS(20, 20);
-            endDrawing();
-        }
+        init();
+        loop();
         closeWindow();
     }
+
+    private void init() {
+        initWindow(800, 450, "Demo");
+        setWindowState(ConfigFlags.FLAG_WINDOW_RESIZABLE);
+        setTargetFPS(60);
+    }
+
+    private void loop() {
+        while (!windowShouldClose()) {
+            beginDrawing();
+            clearBackground(BLACK);
+
+            drawText("X : " + getMousePosition().x() + "|| Y : " + getMousePosition().y(), 0, 160, 1, YELLOW);
+
+            drawText("W : " + getScreenWidth() + "|| H : " + getScreenHeight(), 0, 200, 1, VIOLET);
+
+            drawFPS(0, 0);
+            endDrawing();
+        }
+    }
+
+
 }
